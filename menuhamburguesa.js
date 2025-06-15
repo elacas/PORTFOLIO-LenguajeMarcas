1ccf45e7fa351e4f8c6ee137c909480b2be02bb7
elacas <eli_dkf_@hotmail.com>
2025-06-16 00:02:02 +0200

    ...

--- menuhamburguesa.js
+++ menuhamburguesa.js
@@ -1,14 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.getElementById('enlace_menu');
-    const menu = document.getElementById('menu');
-    
-    hamburger.onclick = function(e) {
-        e.preventDefault();
-        if (menu.style.display === 'flex') {
-            menu.style.display = 'none';
-        } else {
-            menu.style.display = 'flex';
-            menu.style.flexDirection = 'column';
-        }
-    };
-});
+let lanzador = "#enlace_menu";
+let desplegable = "#menu";
+let despliegaClase = "menu_desplegado"
+
+function nav(){
+let lanz = document.querySelector(lanzador);
+lanz.addEventListener("click",despliegaMenu);
+
+}
+
+function despliegaMenu(){
+    let despl = document.querySelector(desplegable);
+    despl.classList.toggle(despliegaClase)
+}
+
+nav();
