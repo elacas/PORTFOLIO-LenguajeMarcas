69daa5b0d636d13f154d928beae4cc23f6c92366
elacas <eli_dkf_@hotmail.com>
2025-06-16 14:13:16 +0200

    16/06

--- popup.js
+++ popup.js
@@ -4,3 +4,9 @@ const btn = document.getElementById("acceptCookies");
 if(!localStorage.cookiesAccepted){
     popup.classList.add("show");
 }
+
+btn.addEventListener("click", function(){
+    localStorage.cookiesAccepted = "true";
+    popup.classList.remove("show");
+});
+
