e45c7303a916b052c1399c3de9097d31fb3f0d42
elacas <eli_dkf_@hotmail.com>
2025-06-16 11:12:06 +0200

    16/06-menu_hamburguesa_centrado_imagen_retrato

--- menuhamburguesa.js
+++ menuhamburguesa.js
@@ -5,12 +5,21 @@ let despliegaClase = "menu_desplegado"
 function nav(){
 let lanz = document.querySelector(lanzador);
 lanz.addEventListener("click",despliegaMenu);
-
 }
 
 function despliegaMenu(){
     let despl = document.querySelector(desplegable);
-    despl.classList.toggle(despliegaClase)
+    let musicButton = document.querySelector("#toggle-musica");
+    
+    despl.classList.toggle(despliegaClase);
+
+    if (despl.classList.contains(despliegaClase)) {
+        musicButton.style.display = 'none';
+    } else {
+        musicButton.style.display = 'block';
+    }
+
 }
 
 nav();
+
