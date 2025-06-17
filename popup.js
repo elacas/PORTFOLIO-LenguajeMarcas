06fd47068269110d83d28423b570fbbb0534c577
elacas <eli_dkf_@hotmail.com>
2025-06-17 16:01:16 +0200

    17/06__POPUP?????

--- popup.js
+++ popup.js
@@ -1,17 +1,15 @@
-document.addEventListener(function() {
+document.addEventListener('DOMContentLoaded', function() {
     const popup = document.getElementById("cookiePopup");
     const btn = document.getElementById("acceptCookies");
     
-    if(!localStorage.cookiesAccepted){
+    if(!localStorage.getItem('cookiesAccepted')){
         popup.classList.add("show");
     }
 
     btn.addEventListener("click", function(){
-        localStorage.cookiesAccepted = "true";
+        localStorage.setItem('cookiesAccepted', 'true');
         popup.classList.remove("show");
     });
 });
-
-
 
 
